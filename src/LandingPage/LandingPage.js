cb6198d2b1317cadf69aa49aee3fc7e8f4029337
Piotr Jagiellowicz <Piotr.Jagiellowicz@gds.ey.com>
2019-12-23 15:04:26 +0100

    added search bar

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { TopNav } from '../TopNav/TopNav';
 import logo from '../Assets/logo.png'
 import styles from './LandingPage.module.css';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 export function LandingPage() {
     return (
         <div>
             <TopNav />
             <img src={logo} alt="belb logo" className={styles.logo}/>
+            <SearchBar />
         </div>
     );
 }
