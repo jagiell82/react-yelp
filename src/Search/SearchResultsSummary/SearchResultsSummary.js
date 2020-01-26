4ee45c71b052ac8acb4bcdacc82da2ab239bb591
Piotr Jagiellowicz <Piotr.Jagiellowicz@gds.ey.com>
2020-01-26 17:01:57 +0100

    new buttons for search filters

--- src/Search/SearchResultsSummary/SearchResultsSummary.js
+++ src/Search/SearchResultsSummary/SearchResultsSummary.js
@@ -8,6 +8,32 @@ export function SearchResultsSummary() {
                 <h1 className="subtitle"><strong>Burgers</strong> Berlin</h1>
                 <p>Showind 1-20 out of 543 results</p>
             </div>
+            <div className={styles.filters}>
+                <button className="button">
+                    <span className="icon">
+                        <i className="fas fa-sliders-h"></i>
+                    </span>
+                    <span>All Fillters</span>
+                </button>
+                <div className="buttons has-addons">
+                    <button className="button">$</button>
+                    <button className="button">$$</button>
+                    <button className="button">$$$</button>
+                    <button className="button">$$$$</button>
+                </div>
+                <button className="button">
+                    <span className="icon">
+                        <i className="fas fa-clock"></i>
+                    </span>
+                    <span>Open Now</span>
+                </button>
+                <button className="button">
+                    <span className="icon">
+                        <i className="fas fa-dollar-sign"></i>
+                    </span>
+                    <span>Cashback</span>
+                </button>
+            </div>
         </div>
     )
 }
